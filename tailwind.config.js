cb5a6547b4464687f50c8b3eb7fc95479a52d92d
Andrei Maxim <andrei@andreimaxim.ro>
2021-10-30 12:58:06 +0300

    Improve the production CSS size

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,8 +49,7 @@ module.exports = {
         },
     },
     plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
     ],
     purge: [
         './src/**/*.liquid'
